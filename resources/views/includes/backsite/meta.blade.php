c104757a52380f5d73a9d2cec18fbe4af3ebdf60
bantolos <bantolo.setiadi@ti.ukdw.ac.id>
2022-10-06 17:10:59 +0700

    update asset update includes

---
+++ resources/views/includes/backsite/meta.blade.php
@@ -0,0 +1,11 @@
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui">
+
+{{-- seo setting base --}}
+<meta name="description" content="{{ 'Meet Your Doctor. Trusted & Proffesional.' }}">
+<meta key="keywords" content="{{ 'doctor, meetdoctotr, patient, appointment, trusted & proffesional' }}">
+<meta name="author" content="{{ 'Meet Doctor' }}">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/includes/frontsite/meta.blade.php
+++ resources/views/includes/frontsite/meta.blade.php
@@ -1,3 +1,8 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+{{-- seo setting base --}}
+<meta name="description" content="{{ 'Meet Your Doctor. Trusted & Proffesional.' }}">
+<meta key="keywords" content="{{ 'doctor, meetdoctotr, patient, appointment, trusted & proffesional' }}">
+<meta name="author" content="{{ 'Meet Doctor' }}">
